ac8e71b640bf2e1c6ea80b26a8e9917219ce91e8
Nick Tirrell <nick@nicktirrell.com>
2018-10-30 14:20:20 -0400

    Updating readme and 404 page.
    -readme
    testing to see if image will display in readme.
    
    -404
    updating 404 to be consistent with other templates.

--- 404.php
+++ 404.php
@@ -1,1 +1,35 @@
-<h1>¯\_(ツ)_/¯ not found ¯\_(ツ)_/¯</h1>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>App Base</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/styles/master.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500" rel="stylesheet">
+</head>
+
+<body class="fadeIn">
+
+    <div class="form">
+
+        <div class="logo">
+            <a href="/"><img src="assets/images/placeholder-logo.png" /></a>
+        </div>
+
+        <div class="success">
+
+            <p>Not found.</p>
+
+            <div class="buttons">
+                <a class="wide" href="/">Home</a>
+            </div>
+
+        </div>
+        
+    </div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/index.js"></script>
+</body>
+
+</html>
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+![alt tag](https://via.placeholder.com/728x90.png "This is a test image")
+
 # PHP App Base
+Need a simple back end and don't know where to start? Start here.
+
+# Setup
+Create an SQL database and update `database.php` accordingly.
+
+Import the contents of `database.sql`.
